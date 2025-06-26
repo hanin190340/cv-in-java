e3af1e3ecdce21d30393884c47f28cb6b5f8db2f
hanin <haninzx2024@gmail.com>
2025-06-26 14:51:45 +0400

    Initial commit of cv.java project

---
+++ helloworldjava.java
@@ -0,0 +1,14 @@
+public class helloworldjava {
+public static void main(String[] args) {
+
+    System.out.println("my name is :hanin alsaddi");
+    System.out.println("phone numer:90199209");
+    System.out.println("Address:muscat seeb");
+    System.out.println("E-mail:haninzx2024@gmail.com");
+    System.out.println("specialty:telecom engineering ");
+    System.out.println("university:national university ");
+    System.out.println("date od birth:26-3-2002 ");
+    System.out.println("Experience:omantel,satMENA");
+    System.out.println("skills:data analysis,communication , teamwork,time management,problem-solving ");
+}
+}
